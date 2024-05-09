f2a4d7715482abde5d9979598db44a5ea992ae26
mpellouin <maxence.pellouin@epitech.eu>
2024-05-09 15:26:39 +0900

    feat(heatmap): generating heatmap from plotly showing the commits

--- src/contributions/contributions.controller.ts
+++ src/contributions/contributions.controller.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, InternalServerErrorException, Query } from '@nestjs/common';
 import { ProvidersService } from '../providers/providers.service';
 import { ContributionsService } from './contributions.service';
+import { NormalizedContributions } from 'src/providers/providers.dto';
 
 @Controller('contributions')
 export class ContributionsController {
@@ -10,7 +11,7 @@ export class ContributionsController {
   ) {}
 
   @Get()
-  async getContribution(@Query('github_id') github_id: string, @Query('gitlab_id') gitlab_id: string): Promise<any> {
+  async getContribution(@Query('github_id') github_id: string, @Query('gitlab_id') gitlab_id: string): Promise<NormalizedContributions> {
     if (!github_id || !gitlab_id) {
       throw new BadRequestException("Missing github_id or gitlab_id query parameter")
     }
@@ -26,5 +27,29 @@ export class ContributionsController {
     );
 
     return totalContributions;
+  }
+
+  @Get('/heatmap')
+  async getHeatmap(@Query('github_id') github_id: string, @Query('gitlab_id') gitlab_id: string): Promise<string> {
+    if (!github_id || !gitlab_id) {
+      throw new BadRequestException("Missing github_id or gitlab_id query parameter")
+    }
+    
+    const githhubContributions = await this.providersService.getContributionsFromGithub(github_id);
+    const gitlabContributions = await this.providersService.getContributionsFromGitlab(gitlab_id);
+
+    const normalizedGithubContributions = this.contributionsService.normalizeGithubContributions(githhubContributions);
+
+    const totalContributions = this.contributionsService.computeTotalContributions(
+      normalizedGithubContributions,
+      gitlabContributions,
+    );
+
+    try {
+    return this.contributionsService.createHeatmap(totalContributions, `${github_id}-${gitlab_id}`);
+    } catch (error) {
+      console.error(error)
+      throw new InternalServerErrorException("Error while creating heatmap");
+    }
   }
 }
--- src/contributions/contributions.service.ts
+++ src/contributions/contributions.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { parse } from 'date-fns';
 import { GithubResponse, NormalizedContributions } from 'src/providers/providers.dto';
+import * as plotly from 'plotly';
 
 @Injectable()
 export class ContributionsService {
+  plotly: any = plotly(process.env.PLOTLY_USER, process.env.PLOTLY_API_KEY);
+  constructor() {
+  }
   normalizeGithubContributions(contributions: GithubResponse): NormalizedContributions {
     const weeks = contributions.contributionsCollection.contributionCalendar.weeks;
     const data = {};
@@ -27,5 +32,78 @@ export class ContributionsService {
     }
 
     return data;
+  }
+
+  async createHeatmap(data: NormalizedContributions, ids: string): Promise<string> {
+    let returnURL = '';
+    const contributionsPerDay = this.computeHeatmapData(data);
+    const totalContributions = Object.values(data).reduce((acc, val) => acc + val, 0);
+    const heatmapData = [
+      {
+        z: contributionsPerDay,
+        y: ['Sat', 'Fri', 'Thu', 'Wed', 'Tue', 'Mon', 'Sun'],
+        x: ['W-52', 'W-51', 'W-50', 'W-49', 'W-48', 'W-47', 'W-46', 'W-45', 'W-44', 'W-43', 'W-42', 'W-41', 'W-40', 'W-39', 'W-38', 'W-37', 'W-36', 'W-35', 'W-34', 'W-33', 'W-32', 'W-31', 'W-30', 'W-29', 'W-28', 'W-27', 'W-26', 'W-25', 'W-24', 'W-23', 'W-22', 'W-21', 'W-20', 'W-19', 'W-18', 'W-17', 'W-16', 'W-15', 'W-14', 'W-13', 'W-12', 'W-11', 'W-10', 'W-9', 'W-8', 'W-7', 'W-6', 'W-5', 'W-4', 'W-3', 'W-2', 'W-1', 'W0'],
+        type: 'heatmap',
+        name: `${ids} total contributions: ${totalContributions}`,
+        colorscale: 'Greens',
+        reversescale: true,
+      }
+    ]
+    const layout = {
+      title: `${ids} total contributions: ${totalContributions}`,
+      xaxis: {
+        title: 'Weeks',
+      },
+      yaxis: {
+        title: 'Days',
+      }
+    }
+    const graphOptions = {
+      fileopt: 'overwrite',
+      filename: ids,
+      layout
+    };
+
+    await new Promise((resolve, reject) => {
+      this.plotly.plot(heatmapData, graphOptions, function (err, msg) {
+          if (err) {
+              reject(err);
+          } else {
+              resolve(msg.url + '.png');
+          }
+      });
+    }).then((url: string) => {
+      returnURL = url;
+    }).catch((error) => {
+      console.error(error);
+      throw new InternalServerErrorException("Error while creating heatmap");
+    });
+
+    return returnURL;
+  }
+
+  computeHeatmapData(data: NormalizedContributions): (number | null)[][] {
+    const contributionsPerDay = [[], [], [], [], [], [], []];
+    Object.keys(data).forEach((date) => {
+      const testDate = parse(date, 'yyyy-MM-dd', new Date());
+      const day = testDate.getDay();
+      if (data[date] === 0) contributionsPerDay[day].push(null);
+      else
+      contributionsPerDay[day].push(data[date]);
+    });
+
+    // Normalization of data so that the heatmap is correctly displayed
+    const firstContributionDay = parse(Object.keys(data)[0], 'yyyy-MM-dd', new Date()).getDay();
+    const lastContributionDay = parse(Object.keys(data)[Object.keys(data).length - 1], 'yyyy-MM-dd', new Date()).getDay();
+    
+    for (let i = 0; i < firstContributionDay; i++) {
+      contributionsPerDay[i].unshift(null);
+    }
+
+    for (let i = lastContributionDay + 1; i < 7; i++) {
+      contributionsPerDay[i].push(null);
+    }
+
+    return contributionsPerDay.reverse();
   }
 }
